bc96356c11de5c9aecd4a608c66fc60352d0ae28
Tanmay <tanmayawasthi105@gmail.com>
2018-08-05 17:30:58 +0530

    Added link function to the directive

--- sample_applications/angular_custom_directives/www/components/item-widget/item-widget.js
+++ sample_applications/angular_custom_directives/www/components/item-widget/item-widget.js
@@ -6,6 +6,16 @@ appModule.directive('itemWidget', [function() {
             item: '=',
             promo: '@',
             pickMe: '&onSelect'
+        },
+        link: function(scope, element, attrs) {
+            //Add event listener for 'click' event
+            element.on('click', function(event) {
+
+                element.html('Thanks for buying this item.');
+                element.css({
+                    color: 'green'
+                });
+            });
         }
     }
 }]);
